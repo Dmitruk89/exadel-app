5f5ccfb13be16783d8846b912af530f379e179a7
Rukavichnikau Dzmitry <drukavichnikau@exadel.net>
2022-01-20 21:14:45 +0300

    refactor: move navigation to separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductsComponent } from './pages/products/products.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './pages/home/home.component';
     ProductsComponent,
     UserPageComponent,
     ContactsComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -12,4 +12,9 @@ export class TopBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  isNavShown = true;
+  toggleNavigation(): void {
+    this.isNavShown = !this.isNavShown;
+  }
+
 }
