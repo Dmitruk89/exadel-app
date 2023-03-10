bbf3b73ae63f49c42d67281205daabe143f9d7be
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-03-10 13:22:36 +0400

    test: add test to footer component

---
+++ src/app/components/footer/footer.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FooterComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contein copyright', () => {
+    const footerElement: HTMLElement = fixture.nativeElement;
+    const footerContainer = footerElement.querySelector('.footer_container')!;
+    expect(footerContainer.textContent).toEqual('© IKEA');
+  });
+});
